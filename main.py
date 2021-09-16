8329f94daec711ff4bf40eba0e827fe199347def
SerDinadan <serdinadan@gmail.com>
2021-09-15 17:30:10 -0700

    read, write, append to file

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-file = open("my_file.txt")
-contents = file.read()
-print(contents)
+# Requires closing file to guarantee resource usage efficiency
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+# Don't need to worry about closing file
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+with open("my_file.txt", mode="a") as file:
+    file.write("\nAppend apple")
